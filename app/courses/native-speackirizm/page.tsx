802df903aa5af0f9e554341b306621ede9104eab
GlebChiz <gchyz@jsdaddy.io>
2023-12-18 01:32:35 +0200

    feat: details about course page

--- app/courses/native-speackirizm/page.tsx
+++ app/courses/native-speackirizm/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Reviews from "components/reviews";
 import Link from "next/link";
 import { useState } from "react";
 import { AppRoutes } from "types/app-routes";
@@ -19,6 +20,74 @@ export default function NativeSpeackirizm() {
     `Explore 53 engaging videos (≈ 7 hours in total), all focused on
   Angular testing. These are expertly crafted by a Google Developer
   Expert in Angular and a Microsoft MVP in Developer Technologies`,
+  ];
+  const forWhomIsThisCourse = [
+    {
+      smile: "bg-yellow-500",
+      text: 'You have some Angular experience but are looking for a "deep dive" into the topic and you want to figure out how everything works under the hood.',
+      bgColor: "bg-blue-100",
+    },
+    {
+      smile: "bg-green-500",
+      text: "Your Angular application consists of a lot of forms, custom controls, and validation rules",
+      bgColor: "bg-blue-300",
+    },
+    {
+      smile: "bg-red-500",
+      text: 'You want to learn how to create custom form controls because you are tired of hacking the Angular Material components and trying to adjust them to the "creative" whims of your UI Designers & Managers',
+      bgColor: "bg-blue-100",
+    },
+  ];
+  const [questionsYouMightHave, setQuestionsYouMightHave] = useState([
+    {
+      title: "At the end of the course 1",
+      text: 'You have some Angular experience but are looking for a "deep dive" into the topic and you want to figure out how everything works under the hood.',
+      showText: false,
+    },
+    {
+      title: "At the end of the course 2",
+      text: 'You have some Angular experience but are looking for a "deep dive" into the topic and you want to figure out how everything works under the hood.',
+      showText: false,
+    },
+    {
+      title: "At the end of the course 3",
+      text: 'You have some Angular experience but are looking for a "deep dive" into the topic and you want to figure out how everything works under the hood.',
+      showText: false,
+    },
+    {
+      title: "At the end of the course 4",
+      text: 'You have some Angular experience but are looking for a "deep dive" into the topic and you want to figure out how everything works under the hood.',
+      showText: false,
+    },
+  ]);
+  const pricingOptions = [
+    {
+      price: "60",
+      name: "Regular price",
+    },
+    {
+      price: "100",
+      name: "Basic",
+    },
+    {
+      price: "60",
+      name: "Advanced",
+    },
+  ];
+  const moreAboutPriceOptions = [
+    {
+      text: "This option gives you lifetime access to all video lectures for the course including also all updates",
+      name: "Regular price",
+    },
+    {
+      text: "You get everything from the 1st option and additionally 1 Mock Interview + Detailed Feedback. It is the best way to find out your level and expose knowledge gaps before the real Angular interview!",
+      name: "Basic",
+      top: true,
+    },
+    {
+      text: "If you are looking for the best and personalized experience - this option is for you! It includes everything from the previous 2 options and additionally 5 hours of personalized training & mentoring sessions. Warning! It may drastically increase your chances of getting a job offer",
+      name: "Advanced",
+    },
   ];
   const expandSubSectionsOfSection = (section: {
     name: string;
@@ -113,14 +182,115 @@ export default function NativeSpeackirizm() {
           </ul>
         </div>
       </section>
-      <h1 className='text-9xl'>Pricing options</h1>
-      <h1 className='text-9xl'>More About Price Options</h1>
-      <h1 className='text-9xl'>For Whom Is This Course</h1>
+      <section className='py-16 text-center'>
+        <h2 className='text-6xl text-blue-900 font-bold mb-2'>
+          For Whom Is This Course
+        </h2>
+        <p className='text-5xl text-blue-500 mb-6'>
+          This course is a perfect choice for you if...
+        </p>
+        <ul className='flex rounded-xl overflow-hidden'>
+          {forWhomIsThisCourse.map((it) => (
+            <li
+              className={`${it.bgColor} flex flex-col justify-center items-center p-12`}
+              key={it.text}
+            >
+              <div className={`${it.smile} rounded-full h-10 w-10 mb-8`}></div>
+              <p className='text-center text-2xl'>{it.text}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className='py-16 text-center'>
+        <h2 className='text-6xl text-blue-900 font-bold mb-2'>
+          Questions You Might Have
+        </h2>
+        <p className='text-5xl text-blue-500 mb-6'>
+          And my honest answers to them
+        </p>
+        <ul className='flex inline-flex flex-col gap-4 max-w-4xl'>
+          {questionsYouMightHave.map((it) => (
+            <li key={it.title} className='bg-blue-100 flex flex-col rounded-xl'>
+              <button
+                onClick={() =>
+                  setQuestionsYouMightHave(
+                    questionsYouMightHave.map((que) => ({
+                      ...que,
+                      showText:
+                        que.title === it.title ? !que.showText : que.showText,
+                    }))
+                  )
+                }
+                className='p-8 flex'
+              >
+                <div className='text-5xl text-blue-900 ml-4'>Arrow</div>
+                <div className='text-5xl text-blue-900 ml-4'>{it.title}</div>
+              </button>
+              {it.showText ? (
+                <div className='p-8 text-2xl'>{it.text}</div>
+              ) : (
+                <></>
+              )}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className='bg-purple-700 text-white flex gap-20 my-40 p-20'>
+        <div>
+          <h2 className='text-5xl font-bold mb-4'>Pricing options</h2>
+          <p className='text-4xl text-white-400'>
+            Pick only what you actually need
+          </p>
+        </div>
+        <ul className='flex flex-col gap-6'>
+          {pricingOptions.map((it) => (
+            <li
+              className='bg-purple-900 rounded-3xl p-12 flex justify-between items-center duration-300 hover:scale-110'
+              key={it.name}
+            >
+              <div>
+                <div className='text-4xl font-bold mb-4'>{it.price}</div>
+                <div className='text-3xl text-white-400'>{it.name}</div>
+              </div>
+              <button className='text-xl h-fit rounded-3xl bg-pink-700 p-4'>
+                Get started now
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className='my-40'>
+        <h2 className='mb-16 text-7xl font-bold text-center text-purple-900'>
+          More About Price Options
+        </h2>
+        <ul className='flex gap-10'>
+          {moreAboutPriceOptions.map((it) => (
+            <li
+              className={`${
+                !it.top ||
+                "bg-purple-100 rounded-xl hover:scale-110 duration-300 relative"
+              } p-10 text-center`}
+              key={it.name}
+            >
+              {it.top ? (
+                <div className='absolute bg-orange-500 text-white px-4 font-bold py-2 right-0 top-0 rounded-xl'>
+                  Top Deal
+                </div>
+              ) : (
+                <></>
+              )}
+              <div className='text-pink-700 text-2xl font-bold mb-10'>
+                {it.name}
+              </div>
+              <div className='text-purple-900 text-xl'>{it.text}</div>
+            </li>
+          ))}
+        </ul>
+      </section>
       <h1 className='text-9xl'>
         здесь ты можешь найти больше бесплатного контента
       </h1>
-      <h1 className='text-9xl'>Questions You Might Have</h1>
-      <h1 className='text-9xl'>Reviews</h1>
+      <Reviews />
     </div>
   );
 }
